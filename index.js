4086bb83b145e3c58dc6dac1c3f46d4732b52146
Calli-T <joy14479@gmail.com>
2021-01-05 10:08:03 +0900

    function

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-alert('How to use JS');
+function greeting(potato) {
+    console.log(potato + "Lv")
+}
+
+greeting(251);
+
+/*const mInfo = {
+    Lv:251,
+    NickName:"Dynast",
+    Server:"Rebute 2",
+    job:["Bishop", "AngelicBuster"],
+    favFood:[{name: "Kimchi", isfatty:true},
+            {name: "Cheese burger", isfatty:true}]
+};
+//alert(mInfo);
+//console.log(mInfo);
+//console.log(mInfo.job[0], mInfo.favFood[1].isfatty);
+console.log(console.log);*/
+
+/*let some = "zeroday";
+const dayOfWeeks = ["Mon", "Tues", "Wed", "Thur", "Fri", "Sat", "Sun", some];
+alert(dayOfWeeks);
+alert(dayOfWeeks[7]);*/
+
+//const는 변경불가능, let은 변경가능 var는 옛날 변수
+
+//alert('How to use JS');
+
+/*let a = 224;
+let b = 4;
+a = a-b;
+
+console.log(b, a);*/
