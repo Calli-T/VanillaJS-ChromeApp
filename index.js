338fe4548b196f4e130f75160bea56389df38e3f
Calli-T <joy14479@gmail.com>
2021-01-05 10:31:16 +0900

    function inner object

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-function greeting(potato) {
+/*const calculator = {
+    plus: function(a, b) {
+        return a+b;
+    }
+}
+
+console.log(calculator.plus(5,5));*/
+
+
+/*function getText(name, job, lv){
+    return `Name: ${name}, job: ${job}, level: ${lv}`;
+}
+console.log(getText("Dynast's Descendant", "AB", 251));
+console.log(getText("UGSDK", "Bishop", 235));
+console.log(getText());*/
+
+
+/*function greeting(name){
+    console.log(`Hello ${name}`);
+}
+greeting("dynast");*/
+//String은 백틱(``)사이에 넣고 그 안에 변수는${변수명}의 형태로 삽입한다
+
+/*function greeting(potato) {
     console.log(potato + "Lv")
 }
 
-greeting(251);
+greeting(251);*/
 
 /*const mInfo = {
     Lv:251,
