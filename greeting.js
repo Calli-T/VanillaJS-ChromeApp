a6d1fe853f69864d7bfd396134ef957297e844da
Calli-T <joy14479@gmail.com>
2021-01-06 15:22:05 +0900

    NameInput

---
+++ greeting.js
@@ -0,0 +1,45 @@
+const form = document.querySelector(".js-form"),
+    input = form.querySelector("input"),
+    greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser",
+    SHOWING_CN = "showing";
+
+function saveName(text){
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    /*위 문장은 기본적으로 적용된 submit이벤트의 행동양식을 없애준다 ex)어디론가 전송하면서 새로고침하기*/
+    const currentValue = input.value;
+    //input의 value가져오기
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
+
+function paintGreeting(text){
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerHTML = `Hello ${text}`;
+}
+
+function loadName(){
+    const currentUser = localStorage.getItem(USER_LS);
+    if(currentUser === null){
+        askForName();
+    } else{
+        paintGreeting(currentUser);
+    }
+}
+
+function init() {
+    loadName();
+}
+
+init();
